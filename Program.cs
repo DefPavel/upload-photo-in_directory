e7d3934228dcdf2a4f8c31c0d95f1dc91f3bd356
DefPavel <33838515+DefPavel@users.noreply.github.com>
2022-02-17 22:08:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+// Регистрация кодировки win 1251 (Потому что в Net. 5 её не существует, нужно регистровать провайдер вручную )
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+Encoding.GetEncoding(1252);
+string Path = @"D:\TEST-IMAGE\";
+
+// Проходимся по директории 
+IEnumerable<string> allfiles = Directory.EnumerateFiles(Path);
+foreach (string filename in allfiles)
+{
+    // Обрезаем путь и берем только файл
+    string file = filename.Split(Path)[1];
+    // Обрезаем формат файла
+    string fullName = file.Split('.')[0];
+    // поиск человека
+    int IdPerson = await ServiceFireBird.GetIdByFullName(fullName);
+    
+    if(IdPerson != 0)
+    {
+        // Конвертируем в массив байтов
+        byte[] photo = await Task.Run(() => ServiceFireBird.GetBytes(filename));
+        // Загружаем на старую базу
+        await ServiceFireBird.UpdateImage(photo, IdPerson);
+    }
+}
+Console.WriteLine("Завершено!");
+Console.ReadKey();
---
+++ ServiceFireBird.cs
@@ -0,0 +1,61 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+public static class ServiceFireBird
+{
+    public static byte[] GetBytes(string fileName)
+    {
+        Image imageToConvert = Image.FromFile(fileName);
+
+        using var ms = new MemoryStream();
+        imageToConvert.Save(ms, format: imageToConvert.RawFormat);
+        return ms.ToArray();
+    }
+
+    /*public static byte[] ConvertImageToByteArray(string fileName)
+    {
+        Bitmap bitMap = new(fileName);
+        ImageFormat bmpFormat = bitMap.RawFormat;
+        Image imageToConvert = Image.FromFile(fileName);
+        using MemoryStream ms = new();
+        imageToConvert.Save(ms, bmpFormat);
+        return ms.ToArray();
+    }
+    */
+    public static async Task<int> GetIdByFullName(string fullName)
+    {
+        string firstName = fullName.Split(' ')[0];
+        string Name = fullName.Split(' ')[1];
+        string lastName = fullName.Split(' ')[2];
+
+        using FbConnection connection = new("database=192.168.250.72:Pers;user=sysdba;password=Vtlysq~Bcgjkby2020;Charset=win1251;");
+        await connection.OpenAsync();
+        await using FbCommand command = new($"select id from sotr where famil = '{firstName}' and name = '{Name}' and otch = '{lastName}'", connection);
+        FbDataReader reader = await command.ExecuteReaderAsync();
+        while (reader.Read())
+        {
+            if (reader.HasRows) 
+                return reader.GetInt32(0);   
+        }
+        return 0;
+    }
+
+
+    public static async Task UpdateImage(byte[] picbyte, int id)
+    {
+
+        using FbConnection connection = new("database=192.168.250.72:Pers;user=sysdba;password=Vtlysq~Bcgjkby2020;Charset=win1251;");
+        await connection.OpenAsync();
+        using FbCommand command = new("UPDATE SOTR SET PHOTO = @photo where id = @id", connection);
+
+        using var cmd = connection.CreateCommand();
+        cmd.CommandText = "UPDATE SOTR SET PHOTO = @photo where id = @id";
+        cmd.Parameters.AddWithValue("@photo", picbyte);
+        cmd.Parameters.AddWithValue("@id", id);
+        _ = await cmd.ExecuteNonQueryAsync();
+
+    }
+}
+
